45fe4b6a89bec0b4c3ef37b9f2a1d2ef68ddd315
Junior Sousa <jrsousadev@gmail.com>
2022-11-01 21:39:12 -0300

    update koa-server-crud

--- src/config.ts
+++
@@ -1,5 +0,0 @@
-export const config = {
-  PORT: process.env.PORT || 9000,
-  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/physio-back',
-  JWT_SECRET: process.env.JWT_KEY || 'secret_key',
-};
